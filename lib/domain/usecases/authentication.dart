f56d4b35ac7ccabba998aff9994fe373f94859d2
Érico Rosiski Weber <33624701+webererico@users.noreply.github.com>
2022-06-08 14:56:26 -0300

    add authentication use case

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,8 @@
+import 'package:vibe/domain/entities/entities.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({
+    required String email,
+    required String password,
+  });
+}
